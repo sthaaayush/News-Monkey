1f195dda74d65e60ec14cad3ee1b580f4bae6d04
Aayush Shrestha <aayushstha20@gmail.com>
2025-07-30 16:45:26 +0545

    added landing page and fixed view for mobile

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import Navbar from './Components/Navbar';
 import NewsComponent from './Components/NewsComponent';
